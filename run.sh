083ebcfd338229b4d9411eb2200f24e9daf1ad19
root <root@unassigned-hostname.unassigned-domain>
2024-01-17 20:52:58 -0500

    feat: add log
    Signed-off-by: Zhiyi Sun <zhiyisun@msn.com>

--- run.sh
+++ run.sh
@@ -1,10 +1,26 @@
-#!/bin/bash -x
+#!/bin/bash -
+num_cpus=$(nproc)
 if [ ! -d log ]; then
 	mkdir log
 fi
 rm -rf log/*
-for i in {1..8} 
-do 
-	python3 deflatebench.py -l /usr/local/bin/qzip -i ${i} > log/zip_${i}.log &
+for threads in 1 4 8 16 32 64 96
+do
+	mkdir -p log/${threads}
+	for i in $(seq 1 ${threads})
+	do 
+		cpu_id=$((${i} % ${num_cpus}))
+		#taskset -c ${cpu_id} python3 deflatebench.py -l /usr/local/bin/qzip -i ${i} > log/${threads}/zip_${i}.log &
+		taskset -c ${cpu_id} python3 deflatebench.py -l /root/zlib-ng/minigzip -i ${i} > log/${threads}/zip_${i}.log &
+	done
+	while true; do
+		sleep 5
+		if pgrep -f "deflatebench.py" > /dev/null; then
+			continue
+		else
+			break
+		fi
+	done
+	echo ${threads} "threads: " >> log/zip.log
+	cat log/${threads}/zip*.log | grep tot >> log/zip.log
 done
-cat log/zip*.log | grep tot
